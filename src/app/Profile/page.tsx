19cf0eb493cd353f608b1e1f978227326e41a40a
ashish-45 <ashishnirvikar5670@gmail.com>
2024-03-27 00:03:15 +0530

    middleware added and helper method created

--- src/app/Profile/page.tsx
+++ src/app/Profile/page.tsx
@@ -3,19 +3,31 @@
 import Axios from "axios";
 import { toast } from "react-toastify";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import Link from "next/link";
 
 export default function ProfilePage() {
   const router = useRouter();
-
+  const [data, setData] = useState("noting");
   const Logout = async () => {
     try {
       const response = await Axios.get("/API/Users/Logout");
       console.log(response, "Response");
-      toast("Logout Success")
+      toast("Logout Success");
       router.push("/Login");
     } catch (err: any) {
       console.log(err, "Logout Failed");
       toast(err.message);
+    }
+  };
+
+  const getUserDetails = async () => {
+    try {
+      const response = await Axios.get("/API/Users/Me");
+      console.log(response.data);
+      setData(response.data.data._id);
+    } catch (err: any) {
+      console.log("User Details Not Found", err);
     }
   };
 
@@ -24,8 +36,22 @@ export default function ProfilePage() {
       <h1>Profile </h1>
       <hr />
       <p>Profile Page</p>
-      <button className="bg-sky-700 p-3 mt-4 rounded-md" onClick={Logout}>
+      <p className="bg-green-600 text-white p-1 rounded">
+        {data === "nothing" ? (
+          "nothing"
+        ) : (
+          <Link href={`/Profile/${data}`}>{data}</Link>
+        )}
+      </p>
+      <button className="bg-red-700 p-3 mt-4 rounded-md" onClick={Logout}>
         Logout
+      </button>
+
+      <button
+        className="bg-sky-700 p-3 mt-4 rounded-md"
+        onClick={getUserDetails}
+      >
+        Get User Details
       </button>
     </div>
   );
